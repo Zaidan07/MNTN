758ce77b8f1a98ce31bbd7035438eb8041a72be1
Zaidan07 <zaidanxgaming@gmail.com>
2024-09-25 11:17:18 +0700

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="32x32"/>
       <body className={`${playfair.variable} ${poppins.variable} antialiased`}>
         <div
           className="absolute"
